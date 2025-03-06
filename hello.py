1e9893a727a5b289ecef97224dbef9bc727a9684
Joshua-Kirkwood <Joshua.Kirkwood@marcellin.school.nz>
2025-03-06 14:45:07 +1300

    Added comments.

--- hello.py
+++ hello.py
@@ -1,11 +1,27 @@
-print("Hello, World!")
+#print("Hello, World!")
 
-name = input("What is your name: ")
-print(f"Kia ora {name}.")
+#name = input("What is your name: ")
+#print(f"Kia ora {name}.")
 
-age = int(input("What is your age: "))
+#age = int(input("What is your age: "))
 
-if age > 25:
-    print("You are old.")
-elif age >= 13 and age <= 19:
-    print("You are a teenager.")
+#if age > 25:
+#    print("You are old.")
+#elif age >= 13 and age <= 19:
+#    print("You are a teenager.")
+
+# if age>= 13 or age <= 19:
+#    print("You might be a teemager")
+
+#umbrella = False
+#sunny = True
+
+#if umbrella and sunny:
+#    print("You think it's going to rain")
+#elif not umbrella and sunny:
+#    print("You think you don't need a umbrella.")
+#elif umbrella and not sunny:
+#    print("It must be raining.")
+#elif not umbrella and not sunny:
+#    print("You must be drenched.")
+
