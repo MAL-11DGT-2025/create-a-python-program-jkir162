86a5c230809be1e5a000a3f73a72cf7d36ac8782
Joshua-Kirkwood <Joshua.Kirkwood@marcellin.school.nz>
2025-03-07 12:54:08 +1300

    Added a new file, worked with list functions.

---
+++ lists.py
@@ -0,0 +1,18 @@
+#colours = ["red", "green", "blue", "white", "black"]
+#print(colours[0])
+#print(colours[1])
+#print(colours[2])
+#print(colours[3])
+#print(colours[4])
+
+#for i in colours:
+#    print(i)
+
+#print(colours[-1])
+
+#print(colours[-3])
+
+#print(len)colours))
+for i in colours:
+    if i == "yellow":
+        print("There is yellow")
